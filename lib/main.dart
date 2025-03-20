16807b72968ed797e581f921000173e8be3edf8b
Daniel Leal <Danicar2na@gmail.com>
2025-03-19 23:00:07 -0500

    correcion bug db

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:recargas_app/views/login_view.dart';
-import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:recargas_app/views/home_view.dart';
+import 'package:recargas_app/views/splash_view.dart';
 
 void main() {
-  sqfliteFfiInit();
-  databaseFactory = databaseFactoryFfi;
-
-  runApp(const ProviderScope(child: MyApp()));
+  runApp(
+    const ProviderScope(
+      child: MyApp(), 
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -17,7 +19,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginView(),
+      home: SplashScreen(),
     );
   }
 }
+
